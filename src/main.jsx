6565258d304ba80fb2509f42e33c9addd14ba306
Ibrahim Mohamed <111082452+ibrahim19201@users.noreply.github.com>
2024-04-20 11:00:31 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,25 @@
 //     <App />
 //   </React.StrictMode>,
 // )
+// import React from 'react';
+// import { createRoot } from 'react-dom';
+// import App from './App.jsx';
+// import './index.css';
+
+// createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
-import { createRoot } from 'react-dom';
-import App from './App.jsx';
+
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
